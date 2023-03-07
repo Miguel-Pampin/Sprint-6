cb8e8ac63a87199e840b60a9fb57177eee378465
Miguel <miguelpampiniglesias@gmail.com>
2023-03-07 19:40:19 +0100

    Ejercicio-4

--- Tutorial/src/app/Components/home/home.component.ts
+++ Tutorial/src/app/Components/home/home.component.ts
@@ -1,20 +1,38 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter, Input } from '@angular/core';
+import { Historia } from '../../interface/interface';
+
 @Component({
   selector: 'app-home',
-  template: `
-    <app-escena [frases]="frases"></app-escena>
-  `
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  frases = [
-    "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
+ mostrar:boolean = false
 
-    "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
+    comenzar():void{
+        this.mostrar = !this.mostrar;
+   
+    }
+  historia:Historia[] = [
+    { 
+        texto:"El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"
+       
+    },
 
-    "L'heroi va decidir travessar la porta que el portava a casa",
-
-    "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..."
-  ];
-}
+    {
+        texto:"Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes."
+       
+    },
+    
+    {
+        texto:"L'heroi va decidir travessar la porta que el portava a casa"
+        
+    },
+    
+    {
+        texto:"Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..."
+           
+    }
+]}
 
 
