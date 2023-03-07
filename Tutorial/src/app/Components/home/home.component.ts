24717bd9522526a2cca11153ca9b6f5e2b048215
Miguel <miguelpampiniglesias@gmail.com>
2023-03-07 23:51:25 +0100

    Ejercicio-6

--- Tutorial/src/app/Components/home/home.component.ts
+++ Tutorial/src/app/Components/home/home.component.ts
@@ -15,23 +15,23 @@ export class HomeComponent {
     }
   historia:Historia[] = [
     { 
-        texto:"El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"
-       
+        texto:"El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
+        img:".../../../assets/1.jpg" 
     },
 
     {
-        texto:"Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes."
-       
+        texto:"Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
+        img: '.../../../assets/2.jpg'
     },
     
     {
-        texto:"L'heroi va decidir travessar la porta que el portava a casa"
-        
+        texto:"L'heroi va decidir travessar la porta que el portava a casa",
+        img:".../../../assets/3.jpg" 
     },
     
     {
-        texto:"Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..."
-           
+        texto:"Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ...",
+        img:".../../../assets/4.jpg"    
     }
 ]}
 
--- Tutorial/src/app/interface/interface.ts
+++ Tutorial/src/app/interface/interface.ts
@@ -1,4 +1,4 @@
 export interface Historia{
     texto: string,
-
+    img:any
 }
